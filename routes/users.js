2cb77d6791020a63865ddcc80cc510b67c4e0e6f
lnanni <x-nani@hotmail.com>
2018-09-07 17:15:10 -0300

    Adicionado o arquivo SQL e mock data para testes

--- routes/users.js
+++ routes/users.js
@@ -1,21 +1,48 @@
 var express = require('express');
 var router = express.Router();
-
+var knex = require('knex')({
+  client: 'mysql',
+  version: '5.7',
+  connection: {
+    host : '127.0.0.1',
+    user : 'root',
+    password : '',
+    database : 'instanews'
+  }
+});
  
 /* GET users listing. */
 router.get('/:id', function(req, res, next) {
-  res.send(JSON.stringify(req.params));
-  next()
+  // res.send(JSON.stringify(req.params));
+  var id = req.params.id;
+  var resp = knex.select().from('users').where("email", id).timeout(1000)
+  
+  .then(rSet =>{
+    res.send(rSet);
+  }) 
+  return 200;
+  // next()
 });
 
 router.get('/', function(req, res, next) {
-  res.send("JSON.stringify(req.params)");
-  next()
+
+  var resp = knex.select('token').from('users').limit(1).timeout(1000).then(rSet =>{
+    res.send(rSet);
+  }) 
+  return 200;
+//  res.send(resp);
+ // next()
 });
 
-router.post('/*', function(req, res) {
-  
-  res.send(JSON.stringify(req.body));
+router.post('/', function(req, res) {
+ var id = req.body.id;
+ var tk = req.body.token;
+  knex("users").where("email", id).update("token",'tk').then();
+  knex.select("*").from('users').where("email", id).timeout(1000)  
+  .then(rSet =>{
+    res.send(rSet);
+  }) 
+  return 200; 
 });
 
 module.exports = router;
